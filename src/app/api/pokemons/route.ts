45ff7b593e74ed1b73f988418493947dc937c2d5
KimDongSin <asstaff2020@gmail.com>
2024-07-10 18:03:48 +0900

    fix: 전체 데이터 검색기능

--- src/app/api/pokemons/route.ts
+++ src/app/api/pokemons/route.ts
@@ -1,45 +1,68 @@
-import { NextResponse } from "next/server";
-import axios from "axios";
+import { NextResponse } from 'next/server';
+import axios from 'axios';
 
 export const TOTAL_POKEMON = 151;
 const PAGE_SIZE = 20;
 
 export const GET = async (request: Request) => {
   const { searchParams } = new URL(request.url);
-  const page = parseInt(searchParams.get("page") || "1");
+  const page = parseInt(searchParams.get('page') || '1');
   const offset = (page - 1) * PAGE_SIZE;
 
   try {
-    const allPokemonPromises = Array.from({ length: PAGE_SIZE }, (_, index) => {
-      const id = index + 1 + offset;
-      if (id > TOTAL_POKEMON) return null;
+    let responseData;
+    if (searchParams.has('page')) {
+      // 페이지네이션 요청일 경우
+      const allPokemonPromises = Array.from({ length: PAGE_SIZE }, (_, index) => {
+        const id = index + 1 + offset;
+        if (id > TOTAL_POKEMON) return null;
 
-      return Promise.all([
-        axios.get(`https://pokeapi.co/api/v2/pokemon/${id}`),
-        axios.get(`https://pokeapi.co/api/v2/pokemon-species/${id}`),
-      ]);
-    }).filter(Boolean);
+        return Promise.all([
+          axios.get(`https://pokeapi.co/api/v2/pokemon/${id}`),
+          axios.get(`https://pokeapi.co/api/v2/pokemon-species/${id}`),
+        ]);
+      }).filter(Boolean);
 
-    const allPokemonResponses = await Promise.all(allPokemonPromises);
+      const allPokemonResponses = await Promise.all(allPokemonPromises);
 
-    const allPokemonData = allPokemonResponses.map(
-      ([response, speciesResponse]: any) => {
-        const koreanName = speciesResponse.data.names.find(
-          (name: any) => name.language.name === "ko"
-        );
+      const allPokemonData = allPokemonResponses.map(([response, speciesResponse]: any) => {
+        const koreanName = speciesResponse.data.names.find((name: any) => name.language.name === 'ko');
         return { ...response.data, korean_name: koreanName?.name || null };
-      }
-    );
+      });
 
-    const totalPages = Math.ceil(TOTAL_POKEMON / PAGE_SIZE);
-    const hasNextPage = page < totalPages;
+      const totalPages = Math.ceil(TOTAL_POKEMON / PAGE_SIZE);
+      const hasNextPage = page < totalPages;
 
-    return NextResponse.json({
-      data: allPokemonData,
-      totalPages,
-      hasNextPage,
-    });
+      responseData = {
+        data: allPokemonData,
+        totalPages,
+        hasNextPage,
+      };
+    } else {
+      // 전체 데이터 요청일 경우
+      const allPokemonPromises = Array.from({ length: TOTAL_POKEMON }, (_, index) => {
+        const id = index + 1;
+
+        return Promise.all([
+          axios.get(`https://pokeapi.co/api/v2/pokemon/${id}`),
+          axios.get(`https://pokeapi.co/api/v2/pokemon-species/${id}`),
+        ]);
+      });
+
+      const allPokemonResponses = await Promise.all(allPokemonPromises);
+
+      const allPokemonData = allPokemonResponses.map(([response, speciesResponse]: any) => {
+        const koreanName = speciesResponse.data.names.find((name: any) => name.language.name === 'ko');
+        return { ...response.data, korean_name: koreanName?.name || null };
+      });
+
+      responseData = {
+        data: allPokemonData,
+      };
+    }
+
+    return NextResponse.json(responseData);
   } catch (error) {
-    return NextResponse.json({ error: "Failed to fetch data" });
+    return NextResponse.json({ error: 'Failed to fetch data' });
   }
 };
--- src/app/pokemonList/page.tsx
+++ src/app/pokemonList/page.tsx
@@ -1,8 +1,7 @@
 "use client";
-
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import type { Pokemon } from "@/types/type.pokemon";
 import Link from "next/link";
 import Image from "next/image";
@@ -15,28 +14,41 @@ import { useSearchStore } from "@/zustand/useSearchStore";
 const ITEMS_PER_PAGE: number = 20;
 
 const PokemonPage: React.FC = () => {
-  const [page, setPage] = useState<number>(1);
-  const [likedPokemons, setLikedPokemons] = useState<number[]>([]);
-  const searchTerm = useSearchStore((state) => state.searchTerm);
+  const [page, setPage] = useState<number>(1); // 페이지네이션 상태 값
+  const [likedPokemons, setLikedPokemons] = useState<number[]>([]); // 좋아요 상태 값
+  const [allPokemons, setAllPokemons] = useState<Pokemon[]>([]); // 전체 포켓몬 데이터 상태 값
+  const searchTerm = useSearchStore((state) => state.searchTerm); // 전역상태 검색 값
 
-  const { data, isPending, error } = useQuery<{
+  // 전체 데이터를 가져오는 함수
+  const fetchAllPokemons = async () => {
+    try {
+      const res = await axios.get("/api/pokemons");
+      const { data } = res.data;
+      setAllPokemons(data); // 전체 포켓몬 데이터 상태 업데이트
+    } catch (error) {
+      console.error("데이터를 불러오는 중 에러가 발생했습니다:", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchAllPokemons(); // 컴포넌트가 마운트될 때 전체 데이터 한 번 가져오기
+  }, []);
+
+  // 페이지네이션 기능
+  const { data: paginatedData, isFetching, error } = useQuery<{
     data: Pokemon[];
     hasNextPage: boolean;
     totalPages: number;
   }>({
     queryKey: ["pokemons", page],
-    queryFn: async (): Promise<{
-      data: Pokemon[];
-      hasNextPage: boolean;
-      totalPages: number;
-    }> => {
+    queryFn: async () => {
       const res = await axios.get(`/api/pokemons?page=${page}`);
-      const { data, hasNextPage, totalPages } = res.data;
-      return { data, hasNextPage, totalPages };
+      return res.data;
     },
+    keepPreviousData: true,
   });
 
-  if (isPending || !data) {
+  if (isFetching && !paginatedData) {
     return <Loading />;
   }
 
@@ -44,8 +56,9 @@ const PokemonPage: React.FC = () => {
     return <div>에러가 발생했습니다.</div>;
   }
 
-  const totalPages: number = Math.ceil(151 / ITEMS_PER_PAGE);
+  const totalPages: number = paginatedData ? Math.ceil(paginatedData.totalPages) : 0;
 
+  // 좋아요 기능
   const toggleLike = (pokemonId: number) => {
     if (likedPokemons.includes(pokemonId)) {
       setLikedPokemons(likedPokemons.filter((id) => id !== pokemonId));
@@ -54,60 +67,72 @@ const PokemonPage: React.FC = () => {
     }
   };
 
-  const filteredPokemons = data.data.filter((pokemon) =>
+  // 필터 기능
+  const filteredPokemons = allPokemons.filter((pokemon) =>
     pokemon.korean_name.includes(searchTerm)
   );
+
+  const displayPokemons = searchTerm ? filteredPokemons : paginatedData?.data ?? [];
 
   return (
     <div className="flex flex-col items-center h-full my-8">
       <h1 className="text-3xl text-center font-bold mt-4 mb-8">Pokémon</h1>
-      <ul className="grid gap-6 justify-center grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-8">
-        {filteredPokemons.map((item) => (
-          <li
-            key={item.id}
-            className="relative flex flex-col items-center p-4 bg-white border-2 border-solid border-gray-200 rounded-lg shadow-md hover:shadow-lg"
-            style={{ maxWidth: "200px" }}
-          >
-            <Link href={`/pokemonList/${item.id}`}>
-              <div className="text-lg font-bold mb-2">No. {item.id}</div>
-              <div className="relative w-32 h-32 mb-2">
-                <Image
-                  src={item.sprites.front_default}
-                  alt={item.name}
-                  layout="fill"
-                  objectFit="contain"
-                />
-              </div>
-              <div className="text-sm text-center font-bold">
-                {item.korean_name}
-              </div>
-            </Link>
-            <button
-              onClick={() => toggleLike(item.id)}
-              className="absolute top-2 right-2 p-2 rounded-full bg-gradient-to-br from-pink-400 to-purple-600 focus:outline-none transform hover:scale-105 transition duration-300"
+      {isFetching && <Loading />}
+      {displayPokemons.length > 0 ? (
+        <ul className="grid gap-6 justify-center grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-8">
+          {displayPokemons.map((item) => (
+            <li
+              key={item.id}
+              className="relative flex flex-col items-center p-4 bg-white border-2 border-solid border-gray-200 rounded-lg shadow-md hover:shadow-lg"
+              style={{ maxWidth: "200px" }}
             >
-              {likedPokemons.includes(item.id) ? (
-                <Image
-                  src={onLike}
-                  alt="좋아요 활성화"
-                  width={30}
-                  height={30}
-                  className="w-6 h-6"
-                />
-              ) : (
-                <Image
-                  src={offLike}
-                  alt="좋아요 비활성화"
-                  width={30}
-                  height={30}
-                  className="w-6 h-6"
-                />
-              )}
-            </button>
-          </li>
-        ))}
-      </ul>
-      <Pagination totalPages={totalPages} page={page} setPage={setPage} />
+              <Link href={`/pokemonList/${item.id}`}>
+                <div className="text-lg font-bold mb-2">No. {item.id}</div>
+                <div className="relative w-32 h-32 mb-2">
+                  <Image
+                    src={item.sprites.front_default}
+                    alt={item.name}
+                    layout="fill"
+                    objectFit="contain"
+                  />
+                </div>
+                <div className="text-sm text-center font-bold">
+                  {item.korean_name}
+                </div>
+              </Link>
+              <button
+                onClick={() => toggleLike(item.id)}
+                className="absolute top-2 right-2 p-2 rounded-full bg-gradient-to-br from-pink-400 to-purple-600 focus:outline-none transform hover:scale-105 transition duration-300"
+              >
+                {likedPokemons.includes(item.id) ? (
+                  <Image
+                    src={onLike}
+                    alt="좋아요 활성화"
+                    width={30}
+                    height={30}
+                    className="w-6 h-6"
+                  />
+                ) : (
+                  <Image
+                    src={offLike}
+                    alt="좋아요 비활성화"
+                    width={30}
+                    height={30}
+                    className="w-6 h-6"
+                  />
+                )}
+              </button>
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <div className="flex items-center justify-center h-full">
+          <span className="text-2xl font-bold">검색 결과가 없습니다</span>
+        </div>
+      )}
+      {!searchTerm && (
+        <Pagination totalPages={totalPages} page={page} setPage={setPage} />
+      )}
     </div>
   );
 };
