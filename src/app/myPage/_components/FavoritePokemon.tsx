ff49977bc2c8265f096ec3222f522bb42145f7a0
t2023-m0020 <t2023-m0020@t2023-m0020ui-MacBookAir.local>
2024-07-10 19:57:13 +0900

    css : 마이페이지 UI 완성

---
+++ src/app/myPage/_components/FavoritePokemon.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import useMypage from "@/zustand/mypageStore";
+import React from "react";
+
+const FavoritePokemon = () => {
+  const { nickname, favoritePokemons } = useMypage();
+  return (
+    <div className="">
+      <h2 className="text-2xl font-bold m-5">{nickname} 님이 찜한 포켓몬</h2>
+      <div className="w-4/5 h-52 bg-[#f2f2f2] m-auto mt-5">
+        {favoritePokemons.map((pokemon, index) => (
+          <div key={index}>{pokemon}</div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default FavoritePokemon;
