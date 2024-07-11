682bdc36da63290a38387dde94015c19e204d539
Sera Choi <c.sera2511@gmail.com>
2024-07-11 19:56:26 +0900

    [style] : update comment st. depens on the index

--- src/app/pokemonList/[id]/_components/PokemonDetailCommentItem.tsx
+++ src/app/pokemonList/[id]/_components/PokemonDetailCommentItem.tsx
@@ -101,39 +101,6 @@ const PokemonDetailCommentItem = ({
           <polygon points="25,0 50,50 0,50" />
         </svg>
       </div>
-
-      {/* 예시! 한 개 더 보일 때는 말꼬리를 반대로... */}
-      {/* <div className="relative w-full bg-white shadow-xl flex mx-auto rounded-xl mb-4">
-        <div className="flex p-4 justify-between w-full">
-          <div className="flex items-center gap-2">
-            <div className="min-w-[100px] min-h-[100px] flex items-center justify-center border-2 rounded-full hidden sm:hidden md:hidden lg:block transition-opacity duration-300 ease-in-out opacity-0 lg:opacity-100">
-              이미지
-            </div>
-            <div className="flex flex-col gap-2">
-              <div className="flex justify-between px-2">
-                <div>닉네임</div>
-                <div>시간</div>
-              </div>
-              <div className="p-2 border-2">포켓몬 귀여와</div>
-            </div>
-          </div>
-          <div className="flex gap-4"> */}
-
-      {/* 본인만 보이게 할 것 */}
-      {/* <div className="flex gap-2 cursor-pointer whitespace-nowrap p-2">
-              <button>수정</button>
-              <button>삭제</button>
-            </div> */}
-
-      {/* </div>
-        </div>
-        <svg
-          className="absolute w-[25%] h-[25%] top-[33%] fill-[#ffffff] left-[-12%] rotate-[270deg]"
-          viewBox="0 0 100 100"
-        >
-          <polygon points="25,0 50,50 0,50" />
-        </svg>
-      </div> */}
     </div>
   );
 };
