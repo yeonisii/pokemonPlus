cfad7a71bfd2c7f565eb0440c9c2913a72237d71
Kyara <kyara0324@naver.com>
2024-07-12 19:03:27 +0900

    update: 메인페이지 ui 구성

--- src/app/components/Loading.tsx
+++ src/app/components/Loading.tsx
@@ -15,7 +15,7 @@ const LoadingWrapper = styled.div`
   right: 0;
   bottom: 0;
   z-index: 100;
-  background-color: #200f50; // 여기서 백그라운드 색깔을 이미지와 동일하게 설정
+  background-color: #200f50;
 `;
 
 const Loading = () => {
@@ -26,11 +26,11 @@ const Loading = () => {
       setIsLoading(false);
     }, 5000);
 
-    return () => clearTimeout(timer); // 컴포넌트 언마운트 시 타이머 클리어
+    return () => clearTimeout(timer);
   }, []);
 
   if (!isLoading) {
-    return null; // 로딩이 끝난 후에는 아무 것도 렌더링하지 않음
+    return null;
   }
 
   return (
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,4 @@
 "use client";
-
 import Link from "next/link";
 import { useState } from "react";
 import Image from "next/image";
@@ -11,40 +10,43 @@ export default function Home() {
 
   return (
     <main className="flex justify-center items-center h-screen">
-      <Link href="/pokemonList">
-        <a
-          onMouseEnter={() => setIsHovered(true)}
-          onMouseLeave={() => setIsHovered(false)}
-          className="m-4 p-4 border-4 border-dotted border-red-400 rounded-xl hover:shadow-md"
-        >
-          <div className="flex flex-col items-center">
-            {isHovered ? (
-              <Image
-                src={openDevice}
-                alt="Opened Device"
-                width={200}
-                height={200}
-              />
-            ) : (
-              <Image
-                src={closeDevice}
-                alt="Closed Device"
-                width={200}
-                height={200}
-              />
-            )}
-            <h1>
-              <span>포</span>
-              <span className="text-red-600">켓</span>
-              <span className="text-yellow-400">몬</span>
-              <span className="text-green-600"> 도감</span>으로
-              <span className="font-bold animate-[slideRight_0.5s_forwards]">
-                {" "}
-                이동
-              </span>
-            </h1>
-          </div>
-        </a>
+      <Link
+        href="/pokemonList"
+        onMouseEnter={() => setIsHovered(true)}
+        onMouseLeave={() => setIsHovered(false)}
+        className="m-4 p-4 rounded-xl"
+      >
+        <div className="flex flex-col items-center">
+          {isHovered ? (
+            <Image
+              src={openDevice}
+              alt="Opened Device"
+              width={500}
+              height={500}
+              className="border-none shadow-none"
+            />
+          ) : (
+            <Image
+              src={closeDevice}
+              alt="Closed Device"
+              width={500}
+              height={500}
+              className="border-none shadow-none"
+            />
+          )}
+          <h1 className="mt-4">
+            {" "}
+            {/* 추가된 여백 */}
+            <span>포</span>
+            <span className="text-red-600">켓</span>
+            <span className="text-yellow-400">몬</span>
+            <span className="text-green-600"> 도감</span>으로
+            <span className="font-bold animate-[slideRight_0.5s_forwards]">
+              {" "}
+              이동
+            </span>
+          </h1>
+        </div>
       </Link>
     </main>
   );
