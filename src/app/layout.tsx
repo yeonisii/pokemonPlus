42a29fe56a4811b727c151d33b4d56e912bea611
DO YEON KIM <rla1eh2dus3@naver.com>
2024-07-12 16:21:50 +0900

    bugfix : dev merge 후 에러 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,11 +12,11 @@ const inter = Inter({ subsets: ["latin"] });
 
 export default function RootLayout({
   children,
-}: {
+}: Readonly<{
   children: React.ReactNode;
-}) {
+}>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body className={inter.className}>
         <Providers>
           {children}
