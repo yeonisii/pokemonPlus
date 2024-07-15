9cd3e7089de1bfbc95a2da453a5f75f118cd2152
t2023-m0020 <t2023-m0020@t2023-m0020ui-MacBookAir.local>
2024-07-15 11:40:05 +0900

    add : 찜한 포켓몬 이미지 추가

--- src/app/myPage/_components/FavoritePokemon.tsx
+++ src/app/myPage/_components/FavoritePokemon.tsx
@@ -2,6 +2,7 @@
 
 import useMypage from "@/zustand/mypageStore";
 import React from "react";
+import Image from "next/image";
 
 const FavoritePokemon = ({ userName }: { userName: string }) => {
   const { nickname, favoritePokemons } = useMypage();
@@ -9,9 +10,15 @@ const FavoritePokemon = ({ userName }: { userName: string }) => {
     <div className="">
       <h2 className="text-2xl font-bold m-5">{userName} 님이 찜한 포켓몬</h2>
       <div className="w-4/5 h-56 bg-[#f2f2f2] m-auto mt-5">
-        {favoritePokemons.map((pokemon, index) => (
+        <Image
+          src="/image/favoritePokemon.png"
+          alt="favorite"
+          width="500"
+          height="500"
+        ></Image>
+        {/* {favoritePokemons.map((pokemon, index) => (
           <div key={index}>{pokemon}</div>
-        ))}
+        ))} */}
       </div>
     </div>
   );
